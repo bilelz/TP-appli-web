80991f78148568ad10330bbbbc5248acaa96e524
bensaid01 <56545429+bensaid01@users.noreply.github.com>
2019-11-24 18:16:50 +0100

    Update form-jquery-validation.js

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -56,21 +56,14 @@
                                             && $("#Adresse").val() !== "" && $("#email").val() !== "" )
                                             {
                                                     $('#myModal').modal("show");
-                                                    $(".modal-title").html("Welcom       "  + document.querySelector("#Prenom").value 
-                                                    +"       "  + document.querySelector("#nom").value     );
-                                                    
-
-                                                    
+                                                    $(".modal-title").html("Bienvenue "+ document.querySelector("#Prenom").value);
                                                     $('.modal-body').html("Vous étes né le: "+ document.querySelector("#datedenaissance").value
-                                                    +"</br>Vous Habitez à: "
+                                                    +"</br>Et vous Habitez à: "
                                                     + document.querySelector("#Adresse").value
-                                                    
                                                     +'</br><a href="https://maps.google.com/maps?q='
-                                                    +("#Adresse").value
-                                                
-                                                    +'"><img src="https://maps.googleapis.com/maps/api/staticmap?markers=' +document.querySelector("#Adresse").value+'&zoom=10&size=470x300&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg"/></a>');
-
-                                                     
+                                                    +document.querySelector("#Adresse").value
+                                                    +'"><img src="https://maps.googleapis.com/maps/api/staticmap?markers='
+                                                    +document.querySelector("#Adresse").value+'&zoom=10&size=470x300&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg"/></a>');
                                             }
                                                         
                                         }
