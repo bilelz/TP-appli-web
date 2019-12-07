16df7bbbc655e80f694400bebeb5bfa7c3dd6103
bensaid01 <56545429+bensaid01@users.noreply.github.com>
2019-12-07 01:01:15 +0100

    Update gps.js

--- tp4/js/gps.js
+++ tp4/js/gps.js
@@ -1,4 +1,5 @@
 // demande de la localisation à l'utilisateur
+
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition, showError);
@@ -10,16 +11,36 @@ function getLocation() {
 // Si l"utilisateur l'autorise, on récupère les coordonnées dans l'objet "position"
 function showPosition(position) {
     var latlon = position.coords.latitude + "," + position.coords.longitude;
-    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="
-    +latlon+"&zoom=14&size=400x300&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg";
-    
-    $("#map").html("<img src='"+img_url+"'>");
+    var img_url = "https://maps.googleapis.com/maps/api/staticmap?markers=" + latlon + "&zoom=14&size=400x300&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg";
+    //$("#adresse").val(latlon);
+    $("#map").html("<img src='" + img_url + "'>");
+
+    var geocoder = new google.maps.Geocoder();
+    var latlng = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+    };
+
+    geocoder.geocode({
+        'location': latlng
+    }, function (results, status) {
+        if (status === 'OK') {
+            if (results[0]) {
+
+                $("#adresse").val(results[0].formatted_address);
+            } else {
+                window.alert('No results found');
+            }
+        } else {
+            window.alert('Geocoder failed due to: ' + status);
+        }
+    });
 }
 
 // Au cas ou l'utilisateur refuse
 // Ou si une erreur arrive
 function showError(error) {
-    switch(error.code) {
+    switch (error.code) {
         case error.PERMISSION_DENIED:
             $("#map").html("User denied the request for Geolocation.");
             break;
@@ -34,7 +55,3 @@ function showError(error) {
             break;
     }
 }
-
-
-
-
