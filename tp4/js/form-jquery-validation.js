3a8005647522f35bb5f90ff99a052e0e2f9f9655
bensaid01 <56545429+bensaid01@users.noreply.github.com>
2019-12-07 01:01:18 +0100

    Update form-jquery-validation.js

--- tp4/js/form-jquery-validation.js
+++ tp4/js/form-jquery-validation.js
@@ -1,95 +1,80 @@
-
-
-  $(document).ready(function () {
+$(document).ready(function () {
     // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
     // voir plus : https://www.w3schools.com/js/js_htmldom.asp
     console.log("DOM ready!");
 
+    // Ccalcuke de nbr de cractaire 
+    $(document).keyup(function () {
+
+        var nombreCaractere = $("#name").val().length;
+
+        var msg = nombreCaractere + ' car';
+        $('#compteur1').text(msg);
+
+        var nombreCaractere2 = $("#firstname").val().length;
+        var msg2 = nombreCaractere2 + ' car';
+        $('#compteur2').text(msg2);
+
+    });
 
 
+    $("#valider").on("click", function store(event) {
+        event.preventDefault();
 
-  // compteur de nbr de caractaire dans les champs 
-  $(document).keyup(function () {
+        var inputNom = document.getElementById("name");
+        var inputPrenom = document.getElementById("firstname");
+        var inputDn = document.getElementById("birth");
+        var inputAdresse = document.getElementById("adresse");
+        var inputEmail = document.getElementById("mail");
 
-    var nombreCaractere = $("#name").val().length;
+        if ($("#name").val() !== "" && $("#firstname").val() !== "" && $("#birth").val() !== "" && $("#adresse").val() !== "" && $("#mail").val() !== "") {
 
-    var msg = nombreCaractere + ' car';
-    $('#compteur1').text(msg);
+            //stocker les valeurs saisie dans le navigateur
+            localStorage.setItem("name", inputNom.value);
+            localStorage.setItem("firstname", inputPrenom.value);
+            localStorage.setItem("birth", inputDn.value);
+            localStorage.setItem("adresse", inputAdresse.value);
+            localStorage.setItem("mail", inputEmail.value);
 
-    var nombreCaractere2 = $("#firstname").val().length;
-    var msg2 = nombreCaractere2 + ' car';
-    $('#compteur2').text(msg2);
+            $('#success').addClass("alert alert-success").text(" Votre formulaire est sauvegardé.");
 
-
-
-
-    $("#valider").on("click",function store(event) { 
-      event.preventDefault();
-      
-          var inputNom= document.getElementById("name");
-          var inputPrenom= document.getElementById("firstname");
-          var inputDn= document.getElementById("birth");
-          var inputAdresse= document.getElementById("adresse");
-          var inputEmail= document.getElementById("mail");
-          
-          if($("#name").val() !== "" && $("#firstname").val() !== "" && $("#birth").val() !== "" && $("#adresse").val() !== "" && $("#mail").val() !== "" ){
-          
+            $("#table").show();
+            $("#liste_des_contacts").show();
 
 
+            // ajout des valeurs saisie dans le tableau
 
-    //stocker les valeurs saisie dans le navigateur
-    localStorage.setItem("name", inputNom.value);
-    localStorage.setItem("firstname", inputPrenom.value);
-    localStorage.setItem("birth", inputDn.value);
-    localStorage.setItem("adresse", inputAdresse.value);
-    localStorage.setItem("mail", inputEmail.value);
-
-    $('#success').addClass("alert alert-success").text("Bravo! le formulaire est sauvegardé.");
-
-    $("#table").show();
-    $("#liste_des_contacts").show();
+        }
 
 
-
-
-  }
-                   
-                  
-});
-
-
-
-   // La géolocalisation vous donnera la latitude et la longitude de l’utilsateur.via la fonction getlocation 
+    });
 
     $("#gps").on("click", function () {
-      getLocation();
+        getLocation();
 
     });
 
     $("#valider").on("click", function () {
-      contactStore.add()
+        contactStore.add()
 
-      var contactList = contactStore.getList();
-      console.log(contactList)
+        var contactList = contactStore.getList();
+        console.log(contactList)
 
-      for (var index in contactList) {
-        console.log(contactList[index].name);
-        document.querySelector("table tbody").innerHTML =
-          document.querySelector("table tbody").innerHTML +
-          '<tr>' +
-          '<td>' + contactList[index].name + '</td>' +
-          '<td>' + contactList[index].firstname + '</td>' +
-          '<td>' + contactList[index].date + '</td>' +
+        for (var index in contactList) {
+            console.log(contactList[index].name);
+            document.querySelector("table tbody").innerHTML =
+                document.querySelector("table tbody").innerHTML +
+                '<tr>' +
+                '<td>' + contactList[index].name + '</td>' +
+                '<td>' + contactList[index].firstname + '</td>' +
+                '<td>' + contactList[index].date + '</td>' +
 
-       
-
-          '<td><a href="https://maps.google.com/maps?q=' + contactList[index].adress + '">' + contactList[index].adress + '</a></td>' +
-          '<td>' + contactList[index].mail + '</td>' +
-          '<tr>';
-      }
+                '<td><a href="https://maps.google.com/maps?q=' + contactList[index].adress + '">' + contactList[index].adress + '</a></td>' +
+                '<td>' + contactList[index].mail + '</td>' +
+                '<tr>';
+        }
     });
 
 
-  });
-});
 
+});
